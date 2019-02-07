8f9dbbf765025d85cca7ab47dcd67f968099a74e
jamie <peebles.jamie1@gmail.com>
2019-02-07 12:00:43 -0600

    Create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -15,21 +15,9 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    // GREETING - individual user
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-    // BROADCAST - let everyone know about new user
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
-
-    socket.on('createMessage', (message) => {
-        console.log('createMessage', message);
-        io.emit('newMessage', generateMessage(message.from, message.text));
-        // socket.broadcast.emit('newMessage', {
-        //     from: message.from,
-        //     text: message.text,
-        //     createdAt: new Date().getTime()
-        // });
-    });
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
