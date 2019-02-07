9f289bba7e19565f66803b0bea12ae946e1cf777
jamie <peebles.jamie1@gmail.com>
2019-02-07 11:59:34 -0600

    Create generateMessage utility

---
+++ server/utils/message.test.js
@@ -0,0 +1,15 @@
+var expect = require('expect');
+
+var { generateMessage } = require('./message');
+
+describe('generateMessage', () => {
+    it('should generate the correct message object', () => {
+        var from = 'Mark';
+        var text = 'Some message';
+        var message = generateMessage(from, text);
+
+        expect(typeof message.createdAt).toBe('number');
+        expect(message).toMatchObject({ from, text });
+
+    });
+});
